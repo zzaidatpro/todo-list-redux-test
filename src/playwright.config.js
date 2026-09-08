1996c3816610e95878b159ab64db45ed0ce184b6
ZAIDAT Zoheir <z.zaidat.pro@gmail.com>
2026-09-08 16:08:14 +0100

    CI/CD with CircleCi - playwright modification

--- src/playwright.config.js
+++ src/playwright.config.js
@@ -2,19 +2,15 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
-  retries: process.env.CI ? 2 : 0,
-
   use: {
-    // Utiliser 127.0.0.1 au lieu de localhost pour éviter les problèmes de résolution DNS sous Linux/Docker
+    // Utiliser 127.0.0.1 au lieu de localhost pour éviter les blocages de résolution DNS sous Linux
     baseURL: 'http://127.0.0.1:5173',
-    trace: 'on-first-retry',
   },
-
   webServer: {
-    // On ajoute --host 0.0.0.0 pour forcer Vite à écouter sur toutes les interfaces réseau
+    // Commande pour démarrer Vite en écoutant sur 0.0.0.0
     command: 'npx vite --host 0.0.0.0 --port 5173',
     url: 'http://127.0.0.1:5173',
     reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
+    timeout: 120 * 1000, // Laisse 2 minutes au serveur pour démarrer dans CircleCI
   },
 });

586b6b9516c8ac2cd4d0e6626e80c18d2f9fd1f8
ZAIDAT Zoheir <z.zaidat.pro@gmail.com>
2026-09-08 17:02:55 +0100

    CI/CD with CircleCi modification 4.1

--- src/playwright.config.js
+++ src/playwright.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   use: {
     // Utiliser 127.0.0.1 au lieu de localhost pour éviter les blocages de résolution DNS sous Linux
     baseURL: 'http://127.0.0.1:5173',
+    trace: 'on-first-retry',
   },
   webServer: {
     // Commande pour démarrer Vite en écoutant sur 0.0.0.0
